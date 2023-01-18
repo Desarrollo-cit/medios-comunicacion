898508fc34f9432c2b731e6cedb1b495e1ce6dc7
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2023-01-18 09:11:58 -0600

    Cambio en la ruta principal

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\AppController;
 $router = new Router();
-$router->setBaseURL('/demo');
+$router->setBaseURL('/medios-comunicacion');
 
 $router->get('/', [AppController::class,'index']);
 
