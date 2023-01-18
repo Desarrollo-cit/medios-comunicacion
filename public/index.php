f92a39053b1e8f4835a2be5745da9375c5192cfd
3ptnDesarrollo <afuentesj1@mindef.mil.gt>
2023-01-18 09:22:36 -0600

    Creación de controller, model y ruta para eventos. Cambio en el layout para enlace a eventos

--- public/index.php
+++ public/index.php
@@ -5,12 +5,14 @@
 require_once __DIR__ . '/../includes/app.php';
 
 
+use Controllers\EventoController;
 use MVC\Router;
 use Controllers\AppController;
 $router = new Router();
 $router->setBaseURL('/medios-comunicacion');
 
 $router->get('/', [AppController::class,'index']);
+$router->get('/eventos', [EventoController::class,'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
