709a568c7288bf289d2755c4224aec2cc41c79d8
Medford Adelso Sotz perez <94406370+Medfordsotz@users.noreply.github.com>
2023-02-03 08:50:22 -0600

    commit armas

--- models/Armas.php
+++ models/Armas.php
@@ -14,7 +14,7 @@ class Armas extends ActiveRecord{
 
     public function __construct($args = []){
         $this->id = $args['id'] ?? null;
-        $this->desc = $args['desc'] ?? '';
+        $this->desc = strtoupper(trim($args['desc'])) ?? '';
         $this->situacion = $args['situacion'] ?? '1';
     }
 
