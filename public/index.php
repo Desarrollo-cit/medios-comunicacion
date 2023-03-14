184c21f0adcc76cab41894c3773599d4cc8cd97d
Alférez Fuentes Juárez <adfjuarez@mindef.mil.gt>
2023-03-13 20:07:16 -0600

    configuración del servidor

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 require_once __DIR__ . '/../includes/app.php';
 date_default_timezone_set('America/Guatemala');
 setlocale(LC_ALL, 'es_ES');
